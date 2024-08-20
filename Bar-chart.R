18b750f9d3fe4adcf562f7e9da7eef485b685fdd
JTIngham <james.t.ingham@outlook.com>
2024-08-20 11:40:55 +0100

    Added title to plot

--- Bar-chart.R
+++ Bar-chart.R
@@ -8,6 +8,7 @@ data(ToothGrowth)
 ?ToothGrowth
 
 ggplot(data = ToothGrowth, aes(x = supp, y = len)) +
-  geom_col(aes(fill = dose), position = "dodge2")
+  geom_col(aes(fill = dose), position = "dodge2") +
+  labs(title = "Guinea Pig Tooth Growth")
 
 # I'd better start giving my guinea pigs some oj...
